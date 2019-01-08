b87ee6eb632785425a81a7fce26509054575f5f8
matheus-cunha <matheus.cunha.sjc@gmail.com>
2019-01-08 20:11:31 -0200

    Initial commit

---
+++ js/global.js
@@ -0,0 +1,44 @@
+// CAROUSEL
+var $item = $('.landing-carousel .carousel-item');
+var $wHeight = $(window).height();
+
+$item.height($wHeight);
+$item.addClass('full-screen');
+
+$('.landing-carousel img').each(function() {
+  var $src = $(this).attr('src');
+  var $color = $(this).attr('data-color');
+  $(this).parent().css({
+    'background-image' : 'url(' + $src + ')',
+    'background-color' : $color
+  });
+  $(this).remove();
+});
+
+$(window).on('resize', function (){
+  $wHeight = $(window).height();
+  $item.height($wHeight);
+
+  // RESPONSIVE TEXTAREA
+  if($wHeight < 500) {
+    $('.form-group textarea').attr('rows', 1);
+  }
+});
+
+// SCROLL
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back-to-top').fadeIn();
+    } else {
+        $('#back-to-top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back-to-top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
